d02aaf6b77b66fcce94110cf4bf270e05c7b4110
Jeremy Bennett <jeremylukebennett@comcast.net>
2018-04-17 15:47:43 -0500

    working on news api

---
+++ type-a-dashboard/src/News.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class News extends React.Component {
+    render() {
+        return (
+
+
+
+        )
+    }
+}
+
+
+export default News;
+
+
+// class News extends React.Component {
+
+    
+
+// }
