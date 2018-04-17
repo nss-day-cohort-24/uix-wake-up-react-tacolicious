cd9c6c1f703c7cf54745b157e3f92ae62fe7169e
Amber Sharpe <amberjsharpe@gmail.com>
2018-04-17 15:44:29 -0500

    added login and logout

--- type-a-dashboard/src/App.js
+++ type-a-dashboard/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import { loginWithGoogle, logout } from './config/helpers';
 import { Button } from 'reactstrap';
 
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div>
+        <header>
         </header>
         <Button onClick={loginWithGoogle} color="primary">Login</Button>
         <Button onClick={logout}>Logout</Button>
