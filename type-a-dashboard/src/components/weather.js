203339aac8c9b91c487f3a31c3222db526a4f0fb
Sam Kimball <jibequan@gmail.com>
2018-04-18 00:00:52 -0500

    Added Dark Sky API call

---
+++ type-a-dashboard/src/components/weather.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+class Weather extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.darksky.net/forecast/Dark Sky API key/37.8267,-122.4233")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log("result", result.daily);
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          Got it!
+        </div>
+      );
+    }
+  }
+};
+
+export default Weather;
