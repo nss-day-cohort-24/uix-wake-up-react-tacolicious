0857accdb40a86178264436a53405bc1e40ca979
Sam Kimball <jibequan@gmail.com>
2018-04-19 16:35:50 -0500

    Added and prepped props for Skycons

--- type-a-dashboard/src/components/daily_weather.js
+++ type-a-dashboard/src/components/daily_weather.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './weather.css';
+import Skycons from 'react-skycons';
 
 const Daily_weather = (props) => {
+	var str = props.icon;
+	var under = str.replace(/-/g, "_");
+	var fixed =under.toUpperCase();
+
 	return(
 		<div className="col-sm daily_weather">
 			<div className="daily_weather_day">Tue</div>
-			<div className="daily_weather_icon">{props.icon}</div>
+			<div className="daily_weather_icon">
+			<Skycons color='white' icon={fixed} autoplay={false}/>
+			</div>
 			<div className="daily_weather_temp">{props.highTemp}</div>
 		</div>
-		)
+		);
 }
 
 export default Daily_weather;
