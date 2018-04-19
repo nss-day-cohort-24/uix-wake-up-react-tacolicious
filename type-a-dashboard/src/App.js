ab5015f1f5fa8538bbac6bf58cf91fe5b3a7ea89
Jeremy Bennett <jeremylukebennett@comcast.net>
2018-04-19 13:58:26 -0500

    news modal opens

--- type-a-dashboard/src/App.js
+++ type-a-dashboard/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import News from './News';
 import Books from './Books';
 // import Example from './API_Creds';
-import ModalExample from './API_Creds';
+import APICreds from './API_CredsModal';
+import NewsModal from './NewsModal';
 import Logbutton from './components/Logbutton';
 import Weather from './components/weather.js';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -13,9 +14,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <News />
-        {/* <Example /> */}
-        <ModalExample />
+        {/* <News /> */}
+        <NewsModal />
+        <APICreds />
         <Books />
         <Logbutton />
         <Weather />
