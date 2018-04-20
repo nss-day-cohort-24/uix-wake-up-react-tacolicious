2cdeaffc0ba625b53f10c31dcc6c5df1e9b28e2b
Sam Kimball <jibequan@gmail.com>
2018-04-20 14:56:41 -0500

    Added performSearch method to be passed to SearchBar

--- type-a-dashboard/src/App.js
+++ type-a-dashboard/src/App.js
@@ -8,22 +8,54 @@ import NewsModal from './NewsModal';
 import BooksModal from './BooksModal';
 import Logbutton from './components/Logbutton';
 import Weather from './components/weather.js';
+import SearchBar from './components/citysearch.js';
 import 'bootstrap/dist/css/bootstrap.css';
+import {googleKey} from './components/weather-key';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      latitude: '25.2048493',
+      longitude: '55.2707828',
+    };
+  }
+
+  performSearch = (query) => {
+    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${googleKey}`)
+    .then(res => res.json())
+    .then(
+      (results) => {
+      let location = results.results[0].geometry.location;
+      this.setState({
+        latitude: location.lat,
+        longitude: location.lng
+      })
+      },
+        
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+  }
+
   render() {
     return (
       <div className="App">
         {/* <News /> */}
-        <div id="menu-btns">
+        {/*<div id="menu-btns">
           <NewsModal />
           <BooksModal />
           <APICreds />
-          {/* <Books /> */}
+          <Books />
           <Logbutton />
-        </div>
-        <Weather />
+        </div>*/}
+        <SearchBar onSearch={this.performSearch}/>
+        <Weather lat={this.state.latitude} lng={this.state.longitude}/>
       </div>
     );
   }
